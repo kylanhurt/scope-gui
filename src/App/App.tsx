4d8aedd33426322abb29b5f484ca29872435488b
kylanhurt <kylan.hurt@gmail.com>
2019-12-22 23:46:29 -0800

    Initial commit

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from '../layout/';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import '../index.css';
+
+import fontAwesomeIcon from "../utility/icons/fontawesome";
+
+
+const App = () => <Layout />;
+
+export default App;
